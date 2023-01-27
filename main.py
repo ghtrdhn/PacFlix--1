872a214b1273305119e7f6a9580790c1ba9c6431
ghtrdhn <r.githubtrdhnr@gmail.com>
2023-01-28 00:29:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from plan import basic_plan, standard_plan, premium_plan
+from plan import basic_plan, standard_plan, premium_plan, Plan
+
+plan_employee = Plan(
+    can_stream=True,
+    can_download=True,
+    has_sd=True,
+    has_hd=True,
+    has_uhd=False,
+    num_devices=2,
+    content="Basic Plan + Sports",
+    price=0,
+    plan_name="Standard Plan",
+    level=2 
+)
+
+print(plan_employee.price)
+print(plan_employee.content)
 
 print(basic_plan.plan_name)
 
